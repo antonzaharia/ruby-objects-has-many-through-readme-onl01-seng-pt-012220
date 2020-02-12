38ec08401d00505896e523860a4958c28b04b651
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-12 14:27:28 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,7 +23,10 @@ class Waiter
   end
   
   def best_tipper
-    meall.map { |meal|
+    best_tipped_meal = meal.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
   end
   
 end
